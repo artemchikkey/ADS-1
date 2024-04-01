5c2ad8e51ef519a97a52c554cace5e51ab2266e5
artemchikkey <160394124+artemchikkey@users.noreply.github.com>
2024-04-01 20:23:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,41 +5,39 @@
 
 bool checkPrime(uint64_t value) {
 for (int i = 2; i * i <= value; i++) {
-		if (value % i == 0) {
-			return false;
-		}
+	if (value % i == 0) {
+		return false;
 	}
-	return true;
+}
+return true;
 }
 
 uint64_t nPrime(uint64_t n) {
 int k = 0;
-	for (uint64_t i = 2; ; i++) {
-		bool is_prime = checkPrime(i);
-		if (is_prime == true) {
-			k++;
-			if (k == n){
-				return i;
-			}
+for (uint64_t i = 2; ; i++) {
+	bool is_prime = checkPrime(i);
+	if (is_prime == true) {
+		k++;
+		if (k == n) {
+			return i;
 		}
 	}
+}
 }
 
 uint64_t nextPrime(uint64_t value) {
 for (uint64_t i = value + 1; ; i++) {
-		bool is_prime = checkPrime(i);
-		if (is_prime == true)
-		return i;
+	bool is_prime = checkPrime(i);
+	if (is_prime == true)
+	return i;
 }
 }
 uint64_t sumPrime(uint64_t hbound) {
 uint64_t sum = 0;
-	for (uint64_t i = 2; i <= hbound ; i++) {
-		bool is_prime = checkPrime(i);
-		if (is_prime == true) {
-			sum = sum + i;
-
-		}
-	}
-	return sum;
+for (uint64_t i = 2; i <= hbound ; i++) {
+	bool is_prime = checkPrime(i);
+	if (is_prime == true) {
+		sum = sum + i;
+    }
 }
+return sum;}
